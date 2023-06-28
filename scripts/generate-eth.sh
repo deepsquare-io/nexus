92ee5de14947e6451972701f74d191f27820c723
Marc Nguyen <nguyen_marc@live.fr>
2023-06-28 16:38:24 +0200

    fix: do not rm rf abi/

--- scripts/generate-eth.sh
+++ scripts/generate-eth.sh
@@ -5,8 +5,7 @@ set -ex
 SCRIPTPATH=$(dirname "$(realpath "$0")")
 PROJECTPATH="${SCRIPTPATH}/.."
 
-rm -rf "${PROJECTPATH}/abi"
-mkdir -p "${PROJECTPATH}/abi"
+rm -f "${PROJECTPATH}/abi/*.ts"
 
 find "${PROJECTPATH}/abi" -name '*.abi' -exec sh -c 'contract=$(basename "$1"); echo "export const ${contract%.abi}Abi = " | cat - "$1" > "${1%.abi}.ts"; echo " as const;" >> ${1%.abi}.ts; rm "$1";' _ {} \;
 
