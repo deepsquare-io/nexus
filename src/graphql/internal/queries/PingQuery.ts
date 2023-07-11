7ce7d5a77e3a86e8fc179780021e2de402aa2352
valentinpollart <valentin@squarefactory.io>
2023-07-11 15:50:30 +0200

    fix: remove async redundant keyword

--- src/graphql/internal/queries/PingQuery.ts
+++ src/graphql/internal/queries/PingQuery.ts
@@ -3,7 +3,7 @@ import { Arg, Query, Resolver } from 'type-graphql';
 @Resolver()
 export default class PingQuery {
   @Query(() => String)
-  async ping(@Arg('pong', { nullable: true }) pong: string | null): Promise<string> {
+  ping(@Arg('pong', { nullable: true }) pong: string | null): string {
     return pong ?? 'pong';
   }
 }
