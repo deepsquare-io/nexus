f2b71c1b9b79956ea0a49b0a36288331c15360ac
Nguyen Marc <nguyen_marc@live.fr>
2023-07-31 15:23:21 +0200

    chore(contracts): use main contracts, do not use beta contracts

--- src/lib/web3/constants/contracts.ts
+++ src/lib/web3/constants/contracts.ts
@@ -8,13 +8,13 @@ import ChainId from '@lib/web3/lib/ChainId';
 
 const mainnet: { [contractName: string]: Address } = {
   Credit: '0x43f185708de9e18D514F317Ca5587F96847A3dbe',
-  FaucetCredit: '0x2a1614Ee3595649916e5C10B3136DcC3aF0F9CD8',
-  FaucetNative: '0xCc49D18f4A79427d3473282D0F1aCCa5Ad4D2312',
-  MetaScheduler: '0x3a97E2ddD148647E60b4b94BdAD56173072Aa925',
-  ProviderManager: '0xb52405C2D11a8d1E4a0c6807f0C169F4D1345447',
-  Tools: '0x7b13A06c4709FAa715AFC5cB429EFdafCFc109d6',
-  Constants: '0x73e364C4F66a7ba0D137027E2ec5D64dccB3453C',
-  ProviderJobQueues: '0x100f3280aD46a766Bd173512dB9c3a8423568De4',
+  FaucetCredit: '0x4e7D6c6394c697aA3378B4F60B534ad6E3398A4d',
+  FaucetNative: '0x1fDAB86aA706Ef4682dc3B236a5094391B0F99A4',
+  MetaScheduler: '0xc9AcB97F1132f0FB5dC9c5733B7b04F9079540f0',
+  ProviderManager: '0x29c60202a1a1d74A7Fcdd40a03f91F06c177c78A',
+  Tools: '0x5fF5520751cEc64b2704942928a09d5bc409EdB3',
+  Constants: '0xCbA35A058FD8f9D83C905F9B66A17D92Ae49363B',
+  ProviderJobQueues: '0xC5590Eb96E6b36103B083e88914226d6a33466E9',
 };
 
 type addressBook = typeof mainnet;
