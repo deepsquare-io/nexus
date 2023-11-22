f96cd921c1f3ea72d0a68d3793c36a79da9794a3
Nguyen Marc <nguyen_marc@live.fr>
2023-11-22 02:26:55 +0100

    chore(npm): remove unused dependencies

--- src/grpc/service.ts
+++ src/grpc/service.ts
@@ -3,7 +3,8 @@
 // Nexus is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 // Nexus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with Nexus. If not, see <https://www.gnu.org/licenses/>.
-import { arrayify } from '@ethersproject/bytes';
+import type { Hex } from 'viem';
+import { fromHex } from 'viem';
 import type { ReadResponse } from '@grpc/generated/logger/v1alpha1/log';
 import type { LoggerAPIClient } from '@grpc/generated/logger/v1alpha1/log.client';
 
@@ -15,7 +16,7 @@ export class GRPCService {
   readAndWatch(
     address: string,
     logName: string,
-    signedHash: string,
+    signedHash: Hex,
     timestamp: number,
   ): Promise<AsyncIterable<ReadResponse>> {
     this.abortReadAndWatch = new AbortController();
@@ -28,7 +29,7 @@ export class GRPCService {
         address: address,
         logName: logName,
         timestamp: BigInt(timestamp),
-        signedHash: arrayify(signedHash),
+        signedHash: fromHex(signedHash, 'bytes'),
       },
       {
         abort: this.abortReadAndWatch.signal,
