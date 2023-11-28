dff20717eb48925507d2e2f1e1991875dc8bb377
Nguyen Marc <nguyen_marc@live.fr>
2023-11-28 01:05:58 +0100

    fix(nexus): if recap if no price

--- src/components/ui/containers/HardwareRecap/HardwareRecap.tsx
+++ src/components/ui/containers/HardwareRecap/HardwareRecap.tsx
@@ -38,7 +38,29 @@ const HardwareRecap: FC<HardwareRecapProps> = ({ defaultDuration, gpuQty = 0, cp
     });
   }, [defaultDuration]);
 
-  const { data: provider } = useGetProviderPrices('0x75761B17c3088ce5Cd8e02575c6DAa438FFA6e12');
+  const readProviderPrices = useGetProviderPrices('0x75761B17c3088ce5Cd8e02575c6DAa438FFA6e12');
+
+  // TODO: HACK: Return fake data on error
+  const provider = useMemo(() => {
+    const { data, error } = readProviderPrices;
+    if (error || !data) {
+      return {
+        cpuPricePerMin: 950000000000000000n,
+        gpuPricePerMin: 8500000000000000000n,
+        memPricePerMin: 80000000000000n,
+      };
+    }
+
+    // Return fake also if everything is zero
+    if (!data.cpuPricePerMin && !data.gpuPricePerMin && !data.memPricePerMin) {
+      return {
+        cpuPricePerMin: 950000000000000000n,
+        gpuPricePerMin: 8500000000000000000n,
+        memPricePerMin: 80000000000000n,
+      };
+    }
+    return data;
+  }, [readProviderPrices]);
 
   const { setValue, formState } = useFormContext<CreditSubformData>();
 
@@ -59,8 +81,6 @@ const HardwareRecap: FC<HardwareRecapProps> = ({ defaultDuration, gpuQty = 0, cp
   useEffect(() => {
     setOpen(!!formState.errors.credit);
   }, [formState.errors]);
-
-  if (!provider) return null;
 
   const rows = [
     ...(gpuQty !== 0
